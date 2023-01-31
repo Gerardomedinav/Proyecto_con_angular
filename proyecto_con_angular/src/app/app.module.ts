75b844d2e49c85958a7cd8914c43a8eab59d80f7
Gerardomedinav <gerardomedinav@hotmail.es>
2023-01-31 09:48:18 -0300

    estructura front end  terminada

--- proyecto_con_angular/src/app/app.module.ts
+++ proyecto_con_angular/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { FotoDePerfilComponent } from './components/foto-de-perfil/foto-de-perfil.component';
@@ -15,13 +14,22 @@ import { NombreComponent } from './components/nombre/nombre.component';
 import { TituloComponent } from './components/titulo/titulo.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { ModalLoginComponent } from './components/modal-login/modal-login.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule} from 'ng-circle-progress';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     FotoDePerfilComponent,
@@ -30,11 +38,22 @@ import { ModalLoginComponent } from './components/modal-login/modal-login.compon
     NombreComponent,
     TituloComponent,
     AccordionComponent,
-    ModalLoginComponent
+    ModalLoginComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    SkillsComponent,
+    IdiomasComponent,
+    EstudiosComponent,
+    ExperienciaLaboralComponent,
+    ProyectosComponent,
+    ContactosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
